980212f5a1068dff0829fdd5bfe85342c52dda94
EdHawkins <elh1964@gmail.com>
2016-05-02 16:40:40 -0500

    Updated exercise_4.js

--- js/exercise_4.js
+++ js/exercise_4.js
@@ -116,7 +116,7 @@ var myLocation = L.mapbox.featureLayer().addTo(map);
       }
       })
     $('directions').fadeIn(400, function(){
-      var summary + data.trip.summary
+      var summary = data.trip.summary
       $('#summary').empty();
       $('#distance').text((Math.round(summary.length*100)/100) +data.trip.units);
       $('#time').text((Math.round(summary.time / 60*100) / 100) + 'min');
